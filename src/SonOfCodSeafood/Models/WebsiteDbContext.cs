e078882d43cb0c06b304e5b7c6b938b9a28fe1c4
Bryant <bryant.m.wang@gmail.com>
2017-02-17 10:39:51 -0800

    Add identity to project, add initial migration.

---
+++ src/SonOfCodSeafood/Models/WebsiteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SonOfCodSeafood.Models
+{
+    public class WebsiteDbContext : IdentityDbContext<WebsiteUser>
+    {
+        public WebsiteDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- src/SonOfCodSeafood/Startup.cs
+++ src/SonOfCodSeafood/Startup.cs
@@ -7,15 +7,35 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SonOfCodSeafood.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace SonOfCodSeafood
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFramework()
+                .AddDbContext<WebsiteDbContext>(options =>
+                    options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            services.AddIdentity<WebsiteUser, IdentityRole>()
+                .AddEntityFrameworkStores<WebsiteDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +47,14 @@ namespace SonOfCodSeafood
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseIdentity();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
