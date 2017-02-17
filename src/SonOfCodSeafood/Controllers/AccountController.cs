3d87cf17604af7a84b08a1a1001dc19ab1f17512
Bryant <bryant.m.wang@gmail.com>
2017-02-17 10:59:52 -0800

    Add ability to register, login, and logout of accounts.

---
+++ src/SonOfCodSeafood/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using SonOfCodSeafood.Models;
+using SonOfCodSeafood.ViewModels;
+using System.Threading.Tasks;
+
+namespace SonOfCodSeafood.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly WebsiteDbContext _db;
+        private readonly UserManager<WebsiteUser> _userManager;
+        private readonly SignInManager<WebsiteUser> _signInManager;
+
+        public AccountController(UserManager<WebsiteUser> userManager, SignInManager<WebsiteUser> signInManager, WebsiteDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var user = new WebsiteUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.failMessage = "Your email or password was wrong or is not registered."
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
