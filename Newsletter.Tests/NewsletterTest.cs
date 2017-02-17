0e8da3be6b0a256cb4c63d868c8cc60e57fafdc8
Bryant <bryant.m.wang@gmail.com>
2017-02-17 11:25:19 -0800

    Add basic test for NewsletterMembers.

---
+++ Newsletter.Tests/NewsletterTest.cs
@@ -0,0 +1,19 @@
+﻿using SonOfCodSeafood.Models;
+using Xunit;
+
+namespace SonOfCod.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetEmailTest()
+        {
+            var member = new NewsletterMember();
+
+            member.Email = "moocow@gmail.com";
+            var result = member.Email;
+
+            Assert.Equal("moocow@gmail.com", result);
+        }
+    }
+}
---
+++ Newsletter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Newsletter.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff820046-4999-4583-9622-57bd2ed725b5")]
