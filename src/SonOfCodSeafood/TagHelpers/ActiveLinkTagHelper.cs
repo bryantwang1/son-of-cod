a253c2b8c9ece6a1e26ea4ac79f66398ff2972e9
Bryant <bryant.m.wang@gmail.com>
2017-02-17 19:09:55 -0800

    Add custom taghelper to set proper links to active in nav, modify nav styling.

---
+++ src/SonOfCodSeafood/TagHelpers/ActiveLinkTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SonOfCodSeafood.TagHelpers
+{
+    public class NavlinkTagHelper : TagHelper
+    {
+        //private const string ActiveActionAttributeName = "active-action";
+        //private const string ActiveControllerAttributeName = "active-controller";
+
+        public string ActiveAction { get; set; }
+        public string ActiveController { get; set; }
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "li";
+
+            if(ViewContext.RouteData.Values["Action"].ToString() == ActiveAction && ViewContext.RouteData.Values["Controller"].ToString() == ActiveController)
+            {
+                output.Attributes.Add("class", "active nav-item");
+            }
+            else
+            {
+                output.Attributes.Add("class", "nav-item");
+            }
+
+        }
+    }
+}
