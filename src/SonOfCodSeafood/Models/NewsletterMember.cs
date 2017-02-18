cb0690d029da9654c21440c9ac999894e12d2285
Bryant <bryant.m.wang@gmail.com>
2017-02-17 16:29:55 -0800

    Add basic styling of navbar and a carousel.

--- src/SonOfCodSeafood/Models/NewsletterMember.cs
+++ src/SonOfCodSeafood/Models/NewsletterMember.cs
@@ -7,9 +7,13 @@ namespace SonOfCodSeafood.Models
     {
         [Key]
         public int NewsletterMemberId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public DateTime Birthday { get; set; }
 
         public NewsletterMember()
